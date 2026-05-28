d84353a726be10339a72e9817c21636f8fc331ff
kuromath <ont337010@gmail.com>
2026-05-28 22:22:31 +0900

    Create Zeta_Mobius_Transform.cpp

---
+++ Set_Function/Zeta_Mobius_Transform.cpp
@@ -0,0 +1,44 @@
+template<typename T>
+vector<T> subset_zeta(int n, vector<T> a) {
+    for (int i = 0; i < n; i++) {
+        for (int T = 0; T < (1 << n); T++) {
+            if (T >> i & 1) {
+                a[T] += a[T ^ (1 << i)];
+            }
+        } 
+    }
+    return a;
+}
+
+template<typename T>
+vector<T> superset_zeta(int n, vector<T> a) {
+    for (int i = 0; i < n; i++) {
+        for (int T = 0; T < (1 << n); T++) {
+            if (!(T >> i & 1)) {
+                a[T] += a[T | (1 << i)];
+            }
+        }
+    }
+}
+
+template<typename T>
+vector<T> subset_mobius(int n, vector<T> a) {
+    for (int i = 0; i < n; i++) {
+        for (int T = 0; T < (1 << n); T++) {
+            if (T >> i & 1) {
+                a[T] -= a[T ^ (1 << i)];
+            }
+        }
+    }
+}
+
+template<typename T>
+vector<T> superset_mobius(int n, vector<T> a) {
+    for (int i = 0; i < n; i++) {
+        for (int T = 0; T < (1 << n); T++) {
+            if (!(T >> i & 1)) {
+                a[T] -= a[T | (1 << i)];
+            }
+        }
+    }
+}
