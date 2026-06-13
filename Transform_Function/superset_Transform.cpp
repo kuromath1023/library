ed3a4946ce663a121b0bd24651e50bb0f3560216
kuromath <ont337010@gmail.com>
2026-06-14 01:40:10 +0900

    changes

--- Transform_Function/superset_Transform.cpp
+++ Transform_Function/superset_Transform.cpp
@@ -1,28 +1,4 @@
 template<typename T>
-vector<T> subset_zeta(int n, vector<T> a) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (1 << n); j++) {
-            if (j >> i & 1) {
-                a[j] += a[j ^ (1 << i)];
-            }
-        } 
-    }
-    return a;
-}
-
-template<typename T>
-vector<T> subset_mobius(int n, vector<T> a) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (1 << n); j++) {
-            if (j >> i & 1) {
-                a[j] -= a[j ^ (1 << i)];
-            }
-        } 
-    }
-    return a;
-}
-
-template<typename T>
 vector<T> superset_zeta(int n, vector<T> a) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < (1 << n); j++) {
