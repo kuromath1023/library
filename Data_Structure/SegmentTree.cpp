96d3cb592e4fcd92cb72d4e448278942e52b7c3a
kuromath <ont337010@gmail.com>
2026-06-29 14:09:36 +0900

    Update SegmentTree.cpp

--- Data_Structure/SegmentTree.cpp
+++ Data_Structure/SegmentTree.cpp
@@ -1,6 +1,5 @@
-template <typename Monoid>
+template <typename S, S(*op)(S, S), S(*e)()>
 struct SegmentTree {
-    using S = typename Monoid::value_type;
     int n, size;
     vector<S> data;
 
@@ -11,12 +10,12 @@ struct SegmentTree {
         n = n_;
         size = 1;
         while (size < n) size <<= 1;
-        data.assign(2 * size, Monoid::e());
+        data.assign(2 * size, e());
     }
 
     void build(const vector<S> &a) {
         for (int i = 0; i < n; i++) data[i + size] = a[i];
-        for (int i = size - 1; i >= 1; i--) data[i] = Monoid::op(data[i << 1], data[i << 1 | 1]);
+        for (int i = size - 1; i >= 1; i--) data[i] = op(data[i << 1], data[i << 1 | 1]);
     }
 
     S get(int k) { return data[k + size]; }
@@ -25,40 +24,40 @@ struct SegmentTree {
         k += size;
         data[k] = x;
         while (k >>= 1) {
-            data[k] = Monoid::op(data[k << 1], data[k << 1 | 1]);
+            data[k] = op(data[k << 1], data[k << 1 | 1]);
         }
     }
 
     S prod(int l, int r) {
         l += size;
         r += size;
-        S vl = Monoid::e(), vr = Monoid::e();
+        S vl = e(), vr = e();
         while (l < r) {
-            if (l & 1) vl = Monoid::op(vl, data[l++]);
-            if (r & 1) vr = Monoid::op(data[--r], vr);
+            if (l & 1) vl = op(vl, data[l++]);
+            if (r & 1) vr = op(data[--r], vr);
             l >>= 1, r >>= 1;
         }
-        return Monoid::op(vl, vr);
+        return op(vl, vr);
     }
 
     template<typename F> int max_right(int l, const F &f) {
         if (l == n) return n;
-        assert(f(Monoid::e()));
+        assert(f(e()));
         l += size;
-        S sum = Monoid::e();
+        S sum = e();
         do {
             while (!(l & 1)) l >>= 1;
-            if (!f(Monoid::op(sum, data[l]))) {
+            if (!f(op(sum, data[l]))) {
                 while (l < size) {
                     l <<= 1;
-                    if (f(Monoid::op(sum, data[l]))) {
-                        sum = Monoid::op(sum, data[l]);
+                    if (f(op(sum, data[l]))) {
+                        sum = op(sum, data[l]);
                         l++;
                     }
                 }
                 return l - size;
             }
-            sum = Monoid::op(sum, data[l]);
+            sum = op(sum, data[l]);
             l++;
         } while ((l & -l) != l);
         return n;
@@ -66,23 +65,23 @@ struct SegmentTree {
 
     template<typename F> int min_left(int r, const F &f) {
         if (r == 0) return 0;
-        assert(f(Monoid::e()));
+        assert(f(e()));
         r += size;
-        S sum = Monoid::e();
+        S sum = e();
         do {
             r--;
             while (r > 1 && (r & 1)) r >>= 1;
-            if (!f(Monoid::op(data[r], sum))) {
+            if (!f(op(data[r], sum))) {
                 while (r < size) {
                     r = r << 1 | 1;
-                    if (f(Monoid::op(data[r], sum))) {
-                        sum = Monoid::op(data[r], sum);
+                    if (f(op(data[r], sum))) {
+                        sum = op(data[r], sum);
                         r--;
                     }
                 }
                 return r + 1 - size;
             }
-            sum = Monoid::op(data[r], sum);
+            sum = op(data[r], sum);
         } while ((r & -r) != r);
         return 0;
     }
