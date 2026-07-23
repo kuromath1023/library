c0d1c10c9ae9c2111c955e3443ff1c01fd748e16
kuromath1023 <{ont337010@gmail.com}>
2026-07-23 20:58:57 +0900

    commits

---
+++ Convolution/AND_Convolution.hpp
@@ -0,0 +1,33 @@
+template<typename T>
+vector<T> superset_zeta(int n, vector<T> a) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (1 << n); j++) {
+            if (!(j >> i & 1)) {
+                a[j] += a[j | (1 << i)];
+            }
+        }
+    }
+    return a;
+}
+
+template<typename T>
+vector<T> superset_mobius(int n, vector<T> a) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (1 << n); j++) {
+            if (!(j >> i & 1)) {
+                a[j] -= a[j | (1 << i)];
+            }
+        }
+    }
+    return a;
+}
+
+template<typename T>
+vector<T> and_convolution(int n, vector<T> a, vector<T> b) {
+    vector<T> za = superset_zeta(n, a);
+    vector<T> zb = superset_zeta(n, b);
+    vector<T> zc(1 << n);
+    for (int i = 0; i < (1 << n); i++) zc[i] = za[i] * zb[i];
+    vector<T> c = superset_mobius(n, zc);
+    return c;
+}
---
+++ Transform_Function/Multiple_Transform.hpp
@@ -0,0 +1,27 @@
+template<typename T>
+vector<T> multiple_zeta(vector<T> a) {
+    int n = a.size();
+    vector<char> isPrime(n, 1);
+    for (int p = 2; p < n; p++) {
+        if (!isPrime[p]) continue;
+        for (int k = 1; k <= (n - 1) / p; k++) {
+            isPrime[k * p] = 0;
+            a[k * p] += a[k];
+        }
+    }
+    return a;
+}
+
+template<typename T>
+vector<T> multiple_mobius(vector<T> a) {
+    int n = a.size();
+    vector<char> isPrime(n, 1);
+    for (int p = 2; p < n; p++) {
+        if (!isPrime[p]) continue;
+        for (int k = (n - 1) / p; k >= 1; k--) {
+            isPrime[k * p] = 0;
+            a[k * p] -= a[k];
+        }
+    }
+    return a;
+}
